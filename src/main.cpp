e61f7b4672c590caaa2e7e8a5ee0062375863f30
Gregory Mevelec <gregory.mevelec.ext@parrot.com>
2017-10-09 13:37:27 +0200

    [DEV][#7] Open an empty sfml window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main(void)
 {
 	std::cout << "Hello World !" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 	return (0);
 }
